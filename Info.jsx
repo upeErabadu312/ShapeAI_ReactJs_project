02bac1545ed102a17cb780b0cd670bdc2c79c9ce
Upeka Erabadupitiya <75311556+upeErabadu312@users.noreply.github.com>
2021-06-27 16:49:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h2>Javascript and React.js</h2>
+      <p>
+        A basic web Dev React Js Bootcamp. We covered some parts of the React
+        and submit project.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
